f2fc17565e2da023da906d01e5a0f88f721fad7d
Bernardo Bastos Leite <bbleite@hotmail.com>
2021-01-19 14:29:38 -0600

    item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,102 @@
-import './App.css';
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import Item from "./components/Item";
+
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          desLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="$69,420"
+          desLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="$40,000"
+          desLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="$83,123"
+          desLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="$112,344"
+          desLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar for New Roofs"
+          desc="Money-back guarantee"
+          desLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Accessories"
+          desc="Money-back guarantee"
+          desLink=""
+          backgroundImg={Accessories}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
